bf78515650b76de9b1904e34703bd317baf2ffe3
ahmedabdelrhman22 <67920927+ahmedabdelrhman22@users.noreply.github.com>
2023-01-12 18:12:58 +0200

    Update Task03.py

--- Python/Task03/Task03.py
+++ Python/Task03/Task03.py
@@ -1,24 +1,25 @@
 opened_Brackets = ["[","{","("]
 closed_Brackets = ["]","}",")"]
 
-def check(Str):
-    stack = []
-    for i in Str:
-        if i in opened_Brackets:
-            stack.append(i)
-        elif i in closed_Brackets:
-            pos = closed_Brackets.index(i)
-            if ((len(stack) > 0) and (opened_Brackets[pos] == stack[len(stack)-1])):
-                stack.pop()
-            else:
-                return "NO"
-    if len(stack) == 0:
-        return "YES"
-    else:
-        return "NO"
+def Check_Balanced_Brackets(Str):
+    stack = []
+    for i in Str:
+        if i in opened_Brackets:
+            stack.append(i)
+        elif i in closed_Brackets:
+                 pos = closed_Brackets.index(i)
+                 if ((len(stack) > 0) and (opened_Brackets[pos] == stack[len(stack)-1])):
+                     stack.pop()
+                 else:
+                     return "NO"
+    if len(stack) == 0:
+        return "YES"
+    else:
+        return "NO"
+
 
 if __name__ == '__main__':
     print("Please enter the brackets")
-    String = input()
-    check(String)
-
+    string = input()
+    Y_or_N = Check_Balanced_Brackets(string)
+    print(Y_or_N)
